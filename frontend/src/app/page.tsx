542c2a0830d37164724a88dcede8851c10dcc3f2
mfwolffe <wolffemf@dukes.jmu.edu>
2025-05-31 19:46:04 -0400

    for jules

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -85,6 +85,7 @@ export default function Home() {
             <h1 className="text-4xl font-bold mb-2">
               JubJubWord
             </h1>
+            <p className='text-gray-500'>for Jules</p>
             <h2 className="text-2xl font-bold mb-2">
               Powered by the JubJub Bird
             </h2>
