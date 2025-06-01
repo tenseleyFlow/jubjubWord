77ae04fc44b061ffa8e3845839cbbd1ed8c42908
espadonne <espadonne@outlook.com>
2025-05-31 23:10:01 -0400

    api, media things

--- backend/jubjub/jubjubword/urls.py
+++ backend/jubjub/jubjubword/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('generate/', views.generate_words, name='generate_words'),
+    path('audio/', views.generate_audio, name='generate_audio'),
     path('health/', views.health_check, name='health_check'),
 ]
